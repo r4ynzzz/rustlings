fbaf6a4657761659d30aa51471308fc1e09644c8
r4ynzzz <ryanttncube@gmail.com>
2026-06-22 16:24:02 +0200

    enums2

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,17 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Move,
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 fn main() {
     println!("{:?}", Message::Resize);
     println!("{:?}", Message::Move);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo("I was here.".to_string()));
+    println!("{:?}", Message::ChangeColor(33, 44, 55));
     println!("{:?}", Message::Quit);
 }
